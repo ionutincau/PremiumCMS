b8d5fdd5456806637174b6272ce440a544fe8e7e
ionutincau <ionut.incau@gmail.com>
2017-03-22 19:28:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Incau Ionut on 22-Mar-17.
+ * Contact: ionut.incau@gmail.com
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
