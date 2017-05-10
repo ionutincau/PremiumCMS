df4191fe29c8184a4ba1f163d4d1708b88410f59
SkReaper <dark_prince.conea@yahoo.com>
2017-05-10 23:44:52 +0300

    Entities Auto_Increment and User_Payment Relationship

--- src/domain/Event.java
+++ src/domain/Event.java
@@ -1,8 +1,7 @@
 package domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Date;
 
 /**
  * Created by ASUS on 04.May.2017.
@@ -10,42 +9,42 @@ import javax.persistence.Id;
 @Entity
 public class Event {
 
-    @Id private int id_event;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) private int id_event;
     @Column private String name;
-    @Column private String start;
-    @Column private String end;
-    @Column private String site;
+    @Column private Date start;
+    @Column private Date end;
+    @Column private String web_page;
     @Column private String location;
     @Column private String description;
-    @Column private int id_abstract;
-    @Column private int id_proposal;
-    @Column private int id_evaluation;
-    @Column private int id_taxes;
+    @Column private Date d_abstract;
+    @Column private Date d_proposal;
+    @Column private Date d_evaluation;
+    @Column private Date d_taxes;
 
     public int getId_event() {return id_event;}
     public String getName() {return name;}
-    public String getStart() {return start;}
-    public String getEnd() {return end;}
-    public String getSite() {return site;}
+    public Date getStart() {return start;}
+    public Date getEnd() {return end;}
+    public String getWeb_page() {return web_page;}
     public String getLocation() {return location;}
 
     public String getDescription() {return description;}
-    public int getId_abstract() {return id_abstract;}
-    public int getId_proposal() {return id_proposal;}
-    public int getId_evaluation() {return id_evaluation;}
-    public int getId_taxes() {return id_taxes;}
+    public Date getD_abstract() {return d_abstract;}
+    public Date getD_proposal() {return d_proposal;}
+    public Date getD_evaluation() {return d_evaluation;}
+    public Date getD_taxes() {return d_taxes;}
 
     public void setId_event(int id_event) {this.id_event = id_event;}
     public void setName(String name) {this.name = name;}
-    public void setStart(String start) {this.start = start;}
-    public void setEnd(String end) {this.end = end;}
-    public void setSite(String site) {this.site = site;}
+    public void setStart(Date start) {this.start = start;}
+    public void setEnd(Date end) {this.end = end;}
+    public void setWeb_page(String web_page) {this.web_page = web_page;}
     public void setLocation(String location) {this.location = location;}
     public void setDescription(String description) {this.description = description;}
-    public void setId_abstract(int id_abstract) {this.id_abstract = id_abstract;}
-    public void setId_proposal(int id_proposal) {this.id_proposal = id_proposal;}
-    public void setId_evaluation(int id_evaluation) {this.id_evaluation = id_evaluation;}
-    public void setId_taxes(int id_taxes) {this.id_taxes = id_taxes;}
+    public void setD_abstract(Date d_abstract) {this.d_abstract = d_abstract;}
+    public void setD_proposal(Date d_proposal) {this.d_proposal = d_proposal;}
+    public void setD_evaluation(Date d_evaluation) {this.d_evaluation = d_evaluation;}
+    public void setD_taxes(Date d_taxes) {this.d_taxes = d_taxes;}
 
 
 }
