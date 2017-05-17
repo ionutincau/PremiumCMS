355b7a08bce7a56c04b38aeda8de55d8ddaa871d
Aurelian <thefrozenone95@gmail.com>
2017-05-17 22:30:04 +0300

    added pending review proposals UI

---
+++ src/Proposals/AddToReviewProposalUI.java
@@ -0,0 +1,55 @@
+package Proposals;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Aurelian on 5/17/2017.
+ */
+public class AddToReviewProposalUI {
+
+    @FXML
+    private Hyperlink proposalDocumentSeeFile;
+
+    @FXML
+    private Label proposalTopicsLabel;
+
+    @FXML
+    private Hyperlink proposalAbstractSeeFile;
+
+    @FXML
+    private Label proposalKeywordsLabel;
+
+    @FXML
+    private Button proposalPassbutton;
+
+    @FXML
+    private Label proposalTypeLabel;
+
+    @FXML
+    private Label proposalDocumentFilename;
+
+    @FXML
+    private Button proposalReviewButton;
+
+    @FXML
+    private Label proposalNameLabel;
+
+    @FXML
+    private Label proposalAbstractFilename;
+
+    @FXML
+    private Label proposalSessionLabel;
+
+    public AddToReviewProposalUI()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+}
---
+++ src/Proposals/PendingProposalsUI.java
@@ -0,0 +1,27 @@
+package Proposals;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * Created by Aurelian on 5/17/2017.
+ */
+public class PendingProposalsUI {
+
+    @FXML
+    private ListView<String> proposalsListView1;
+
+    @FXML
+    private Button proposalViewButton;
+
+    public PendingProposalsUI()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+}
