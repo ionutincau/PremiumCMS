d7362a6dd09da38f66671ffcb407bf1b744ebb51
Aurelian <thefrozenone95@gmail.com>
2017-05-18 13:29:48 +0300

    added pending upcoming sessions tab UI

---
+++ src/Sessions/UpcomingSessionsTabUIController.java
@@ -0,0 +1,27 @@
+package Sessions;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * Created by Aurelian on 5/18/2017.
+ */
+public class UpcomingSessionsTabUIController {
+
+    @FXML
+    private Button upcomingSessionParticipateButton;
+
+    @FXML
+    private ListView<String> upcomingSessionsListView;
+
+    public UpcomingSessionsTabUIController()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+}
