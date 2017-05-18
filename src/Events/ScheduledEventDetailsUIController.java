5654d6c811b07f825889fb78a85280bb58529599
Aurelian <thefrozenone95@gmail.com>
2017-05-19 01:05:30 +0300

    added proposals evaluation UI added events schedule  UI added transaction approved UI

---
+++ src/Events/ScheduledEventDetailsUIController.java
@@ -0,0 +1,39 @@
+package Events;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Aurelian on 5/18/2017.
+ */
+public class ScheduledEventDetailsUIController {
+
+    @FXML
+    private Label eventSessionLabel;
+
+    @FXML
+    private Label eventTimeLabel;
+
+    @FXML
+    private Label eventRoomLabel;
+
+    @FXML
+    private Hyperlink eventSummaryFileLink;
+
+    @FXML
+    private Label eventSpeakerLabel;
+
+    @FXML
+    private Label eventDateLabel;
+
+    public ScheduledEventDetailsUIController()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+}
---
+++ src/Proposals/EvaluateProposalsTabUIController.java
@@ -0,0 +1,27 @@
+package Proposals;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * Created by Aurelian on 5/19/2017.
+ */
+public class EvaluateProposalsTabUIController {
+
+    @FXML
+    private ListView<String> evaluateProposalsListView;
+
+    @FXML
+    private Button proposalEvaluateButton;
+
+    public EvaluateProposalsTabUIController()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+}
